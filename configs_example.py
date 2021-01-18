b086b6baae03e38be6f1e1255862a46a4eff4a2e
Lookingforcommit <61158429+Lookingforcommit@users.noreply.github.com>
2021-01-18 21:59:00 +0300

    Create configs_example.py

---
+++ configs_example.py
@@ -0,0 +1,3 @@
+token = ""  # Insert your bot token here
+path_to_weights = ""  # Path to tne model weights on your computer
+work_directory = ""  # Path to the project working directory. It will be used for savin temporary files
